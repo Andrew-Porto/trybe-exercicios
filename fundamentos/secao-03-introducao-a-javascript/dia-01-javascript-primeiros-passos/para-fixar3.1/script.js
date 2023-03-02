5ce8b91d29808770d4ebed9230b4dbe521b28168
Andrew Porto da Costa <intheand@outlook.com>
2023-03-02 20:46:06 -0300

    Adiciona um exercício de fixação bônus

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/para-fixar3.1/script.js
@@ -0,0 +1,29 @@
+// let comida = 'pão na chapa';
+// let bebida = 'café';
+
+// if (bebida === 'café' && comida === 'pão na chapa') {
+//   console.log('Muito obrigado(a) pela refeição :)');
+// } else {
+//   console.log('Acho que houve um engano com meu pedido');
+// }
+
+// let conditionOne = true;
+// let conditionTwo = false;
+
+// console.log(conditionOne && conditionTwo);
+
+// let cenouras = true;
+// let leite = true;
+// let arroz = true;
+// let feijao = false;
+
+// let listaDeCompras = cenouras && leite && arroz && feijao;
+
+// console.log(listaDeCompras);
+
+// console.log(true && true); // true
+// console.log(true && false); // false
+// console.log(false && true); // false
+// console.log(false && false); // false
+
+// console.log(10 + 5 * 5); // 10 + 25
