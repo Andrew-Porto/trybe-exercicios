d9150dad6d331c26ffa59f57a737f65a1870eebf
Andrew Porto da Costa <intheand@outlook.com>
2023-03-02 23:04:51 -0300

    Adiciona o primeiro exercício e renomeia os outros diretórios

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/para-fixar-06/script.js
@@ -0,0 +1,15 @@
+let pessoaCandidata = "unknown";
+
+switch (pessoaCandidata) {
+  case "Aprovada":
+    console.log("Parabéns, você está no grupo de pessoas aprovadas!");
+    break;
+  case "Lista":
+    console.log("Você está na nossa lista de espera.");
+    break;
+  case "Reprovada":
+    console.log("Infelizmente, você reprovou.");
+    break;
+  default:
+    console.log("Informação incorreta.");
+}
