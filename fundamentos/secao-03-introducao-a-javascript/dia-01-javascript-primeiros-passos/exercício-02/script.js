02ea40e95c9c59f136fdc7306474b9e18a8503a8
Andrew Porto da Costa <intheand@outlook.com>
2023-03-02 23:03:37 -0300

    Adiciona o primeiro exercício

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercício-02/script.js
@@ -0,0 +1,17 @@
+let primeiro = 1;
+let segundo = 1;
+let maior;
+
+if (primeiro > segundo){
+    maior = primeiro
+    console.log(maior);    
+}
+
+else if (segundo > primeiro){
+    maior = segundo
+    console.log(maior);
+}
+
+else {
+    console.log('Os número são iguais');
+}
