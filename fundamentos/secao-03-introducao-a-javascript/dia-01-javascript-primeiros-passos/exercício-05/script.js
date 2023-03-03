05e532f26051461f91165de68b03fff8b7d1a3b7
Andrew Porto da Costa <intheand@outlook.com>
2023-03-03 18:37:29 -0300

    Adiciona o execício de número 5

--- fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercício-05/script.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercício-05/script.js
@@ -1,14 +1,23 @@
 let primeiro = 1;
 let segundo = 89;
 let terceiro = 90;
-let verificacao = primeiro > 0 && segundo > 0 && terceiro > 0;
+//Gabarito
+// let verificacao = primeiro > 0 && segundo > 0 && terceiro > 0;
 
-if (verificacao) {
-  if (primeiro + segundo + terceiro == 180) {
-    console.log(true);
-  } else {
-    console.log(false);
-  }
-} else {
+// if (verificacao) {
+//   if (primeiro + segundo + terceiro == 180) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// } else {
+//   console.log("Error 404!");
+// }
+
+if (primeiro <= 0 || segundo <= 0 || terceiro <= 0) {
   console.log("Error 404!");
+} else if (primeiro + segundo + terceiro == 180) {
+  console.log(true);
+} else {
+  console.log(false);
 }
