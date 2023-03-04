167733a5168543b90ecb7f62c6903908b9793145
Andrew Porto da Costa <intheand@outlook.com>
2023-03-03 22:26:53 -0300

    Adiciona o execício de número 10

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercício-10/script.js
@@ -0,0 +1,16 @@
+let custo = 10;
+let valorVenda = 30;
+let imposto = valorVenda * 20 / 100;
+let lucro = valorVenda - imposto;
+
+if (custo === 0 || valorVenda === 0) {
+    console.log('Error 404: Valores de custo ou venda inválidos!');
+}
+else if (lucro = lucro * 1000) {
+    console.log(lucro);
+}
+
+
+
+
+
