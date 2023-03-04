48c7b5652bfd5b21b44d55a0d97bdbb4851c5e21
Andrew Porto da Costa <intheand@outlook.com>
2023-03-03 21:15:06 -0300

    Adiciona o execício de número 7

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercício-07/script.js
@@ -0,0 +1,17 @@
+let porcentagem = 101;
+
+if (porcentagem < 0 || porcentagem > 100) {
+  console.log("Error 404: nota inválida!");
+} else if (porcentagem >= 90) {
+  console.log("A");
+} else if (porcentagem >= 80) {
+  console.log("B");
+} else if (porcentagem >= 70) {
+  console.log("C");
+} else if (porcentagem >= 60) {
+  console.log("D");
+} else if (porcentagem >= 50) {
+  console.log("E");
+} else {
+  console.log("F");
+}
