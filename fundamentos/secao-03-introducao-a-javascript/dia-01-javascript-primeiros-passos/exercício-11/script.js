8c019f2bafa36b871e014b0c6184bd5d88664f53
Andrew Porto da Costa <intheand@outlook.com>
2023-03-05 01:08:00 -0300

    Finaliza exercício 11

--- fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercício-11/script.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercício-11/script.js
@@ -1,33 +1,60 @@
-let bruto = 2594.92;
+let bruto = 0;
 let inss;
 let iR;
 let liquido;
+console.log("Salário Bruto");
+console.log(bruto);
 
-if (bruto <= 1556.94) {
-  liquido = (bruto * 8) / 100;
-  console.log(liquido.toFixed(2));
+//cálculo do valor de desconto inss
+
+if (bruto <= 1556.94 && bruto > 0) {
+  inss = (bruto * 8) / 100;
+  console.log("Valor de desconto inss");
+  console.log(inss.toFixed(2));
 } else if (bruto <= 2594.92 && bruto > 1556.95) {
   inss = (bruto * 9) / 100;
-  console.log(bruto.toFixed(2));
+  console.log("Valor de desconto inss");
+  console.log(inss.toFixed(2));
 } else if (bruto <= 5189.82 && bruto > 2594.93) {
-    inss = (bruto * 11) / 100;
-    iR = inss
-
+  inss = (bruto * 11) / 100;
+  console.log("Valor de desconto inss");
+  console.log(inss.toFixed(2));
 } else if (bruto > 5189.82) {
-    inss = (bruto - 570.88);
+  inss = bruto - 570.88;
+  console.log("Valor de desconto inss");
+  console.log(inss.toFixed(2));
+} else {
+  console.log("Error 404: Valor inválido!");
+  
 }
 
-else {
-console.log(inss.toFixed(2));
-}
-
-if (bruto = bruto - inss) {
-    
-    if ((bruto >= 1903.99 && inss < 2826.65)) {
+//cálculo do valor de desconto IR
+if ((bruto = bruto - inss && bruto > 0)) {
+  if (bruto >= 1903.99 && inss < 2826.65) {
     iR = (bruto * 7.5) / 100;
+    console.log("Valor de desconto IR");
     console.log(iR.toFixed(2));
-    }
-
+  } else if (bruto > 1556.95 && bruto < 2594.92) {
+    console.log("Valor de salário a ser recebido");
+  } else if (bruto >= 2826.66 && inss < 3751.05) {
+    iR = (bruto * 15) / 100;
+    console.log("Valor de desconto IR");
+    console.log(iR.toFixed(2));
+  } else if (bruto >= 3751.06 && inss < 4664.68) {
+    iR = (bruto * 22.5) / 100;
+    console.log("Valor de desconto IR");
+    console.log(iR.toFixed(2));
+  } else if (bruto >= 4664.68) {
+    iR = (bruto * 27.5) / 100;
+    console.log("Valor de desconto IR");
+    console.log(iR.toFixed(2));
+  }
+} else {
+  console.log("Abaixo do valor minimo para desconto do imposto de renda!");
 }
 
-//if (liquido = bruto - iR) 
+//cálculo do valor liquido
+if ((liquido = bruto - iR)) {
+  console.log("Valor de salário a ser recebido");
+  console.log(liquido.toFixed(2));
+}
