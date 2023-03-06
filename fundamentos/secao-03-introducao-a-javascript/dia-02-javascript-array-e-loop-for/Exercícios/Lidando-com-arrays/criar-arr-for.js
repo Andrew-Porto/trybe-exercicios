288e7bd6a4c1027dd328ed748f2ba46059c96d25
Andrew Porto da Costa <intheand@outlook.com>
2023-03-06 00:08:08 -0300

    adiciona exercícios com arrays e bubble sort

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/Exercícios/Lidando-com-arrays/criar-arr-for.js
@@ -0,0 +1,11 @@
+let arr = [];
+let div = 0;
+
+//criando array através do for.
+
+for (let index = 0; index <= 25; index += 1) {
+  arr.push(index);
+
+  //dividindo por 2 os elementos do array criado.
+  console.log(arr[index] / 2);
+}
---
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/Exercícios/Lidando-com-arrays/inverter.js
@@ -0,0 +1,7 @@
+let word = 'tristeza';
+let sad = '';
+
+for(let index = 0; index < word.length; index += 1){
+    sad += word [word.length - 1 - index]
+}
+console.log(sad);
